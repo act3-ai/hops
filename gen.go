727f4640070425ccd21cfd1345bf8a9647cef10e
Justen Stall <39888103+justenstall@users.noreply.github.com>
2024-06-04 20:13:31 +0000

    add easyjson go:generate
    Signed-off-by: Justen Stall <39888103+justenstall@users.noreply.github.com>

--- gen.go
+++ gen.go
@@ -5,7 +5,4 @@ package hops
 //go:generate /usr/bin/env bash -c "NO_COLOR=1 go run ./cmd/hops gendocs md --only-commands docs/cli/"
 
 // Generates performant JSON Marshal/Unmarshal functions for structs that avoid reflection.
-//go:generate go run github.com/mailru/easyjson/easyjson@master -all -pkg -- ./internal/apis/formulae.brew.sh/v1
-//go:generate go run github.com/mailru/easyjson/easyjson@master -all -pkg -- ./internal/apis/formulae.brew.sh/v2
-//go:generate go run github.com/mailru/easyjson/easyjson@master -all -pkg -- ./internal/apis/formulae.brew.sh/v3
-//go:generate go run github.com/mailru/easyjson/easyjson@master -all -pkg -- ./internal/apis/formulae.brew.sh/common
+//go:generate go run github.com/mailru/easyjson/easyjson@master -all -pkg -- ./internal/apis/formulae.brew.sh/v1  ./internal/apis/formulae.brew.sh/v2 ./internal/apis/formulae.brew.sh/v3 ./internal/apis/formulae.brew.sh/common
